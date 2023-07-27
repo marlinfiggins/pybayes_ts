bfe4e74c9f96e2fdade7288f91c06a0835fc077a
marlinfiggins <marlinfiggins@gmail.com>
2023-07-27 13:55:14 -0700

    More observation likelihoods

--- bayests/LikelihoodTransform.py
+++ bayests/LikelihoodTransform.py
@@ -18,3 +18,38 @@ class Normal(ObservationLikelihood):
     def model(self, data, pred_raw):
         sigma = numpyro.sample("sigma", dist.HalfNormal(self.rssd))
         numpyro.sample("obs", dist.Normal(pred_raw, sigma), obs=data)
+
+
+class NegativeBiniomial(ObservationLikelihood):
+    def __init__(
+        self, raw_alpha_sd=0.01
+    ):  # Want option to do by multiple dimensions
+        self.rasd = raw_alpha_sd
+
+    def model(self, data, pred_raw):
+        raw_alpha = numpyro.sample(
+            "raw_alpha", dist.HalfNormal(0.1, self.rasd)
+        )
+        numpyro.sample(
+            "obs",
+            dist.NegativeBinomial2(
+                jnp.exp(pred_raw), jnp.power(raw_alpha, -2)
+            ),
+            obs=data,
+        )
+
+
+class Poisson(ObservationLikelihood):
+    def __init__(self):
+        pass
+
+    def model(self, data, pred_raw):
+        numpyro.sample("obs", dist.Poisson(jnp.exp(pred_raw)), obs=data)
+
+
+class Binomial(ObservationLikelihood):
+    def __init__(self):
+        pass
+
+    def model(self, data, N, pred_raw):
+        numpyro.sample("obs", dist.BinomialLogits(N, pred_raw), obs=data)
