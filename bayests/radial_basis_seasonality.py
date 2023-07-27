e64a5e01b2e6666a9180d8b18c2dd66d6d392251
marlinfiggins <marlinfiggins@gmail.com>
2023-07-27 14:01:06 -0700

    Adding RadialBasisSeasonality

---
+++ bayests/radial_basis_seasonality.py
@@ -0,0 +1,35 @@
+import jax.numpy as jnp
+from bayests.TimeSeriesTree import TimeSeriesNode
+import numpyro
+import numpyro.distributions as dist
+
+
+class RadialBasisSeasonality(TimeSeriesNode):
+    def __init__(self, k, s=None, name=None, period=365.25, alpha=1):
+        assert k is not None or s is not None, (
+            "Linear trend must have either given a num of change points `k`"
+            + "or change points `s` directly."
+        )
+        super().__init__()
+        self.s = s  # Centers of basis elements
+        self.k = k if s is None else len(s)  # Number of basis elements
+        self.period = period
+        self.alpha = alpha
+        self.n_params = k
+        self.name = name or f"RadialBasisSeasonality(k={k})"
+
+    @staticmethod
+    def _design(s, alpha, period, t):
+        t_mod = jnp.remainder(t, period)
+        return jnp.exp(jnp.power((t_mod[:, None] - s) / alpha, -2))
+
+    def design(self, t):
+        if self.s is None:
+            self.s = jnp.linspace(jnp.min(t), jnp.max(t), num=self.k)
+        self.features = self._design(self.s, self.alpha, self.period, t)
+
+    def _model(self, t):
+        with numpyro.plate(self.name + f"_plate_{self.k}", self.n_params):
+            beta = numpyro.sample(self.name + "_beta", dist.Normal(0.0, 1.0))
+        self.design(t)
+        self.output = jnp.dot(self.features, beta)
